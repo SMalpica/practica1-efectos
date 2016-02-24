f9c6b9187dcf9a623148330899887dc81f7b207d
Sandra Malpica <sandra_malpica@hotmail.com>
2016-02-24 13:50:13 +0100

    Captura de imagenes y layout basico para la edicion

--- openCVTutorial1CameraPreview/build/generated/source/r/debug/org/opencv/samples/tutorial1/R.java
+++ openCVTutorial1CameraPreview/build/generated/source/r/debug/org/opencv/samples/tutorial1/R.java
@@ -42,12 +42,23 @@ containing a value of this type.
     public static final class id {
         public static final int any=0x7f050000;
         public static final int back=0x7f050001;
+        public static final int btnAlien=0x7f050007;
+        public static final int btnCaptura=0x7f05000b;
+        public static final int btnContraste=0x7f050006;
+        public static final int btnDistorsion=0x7f050009;
+        public static final int btnOtros=0x7f05000a;
+        public static final int btnPoster=0x7f050008;
+        public static final int captureButton=0x7f05000e;
+        public static final int editedImage=0x7f050004;
         public static final int front=0x7f050002;
-        public static final int tutorial1_activity_java_surface_view=0x7f050003;
-        public static final int tutorial1_activity_native_surface_view=0x7f050004;
+        public static final int layoutBotones=0x7f050005;
+        public static final int originalImage=0x7f050003;
+        public static final int tutorial1_activity_java_surface_view=0x7f05000c;
+        public static final int tutorial1_activity_native_surface_view=0x7f05000d;
     }
     public static final class layout {
-        public static final int tutorial1_surface_view=0x7f030000;
+        public static final int captured_layout=0x7f030000;
+        public static final int tutorial1_surface_view=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- openCVTutorial1CameraPreview/src/main/java/org/opencv/samples/tutorial1/Tutorial1Activity.java
+++ openCVTutorial1CameraPreview/src/main/java/org/opencv/samples/tutorial1/Tutorial1Activity.java
@@ -4,18 +4,36 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Tutorial1Activity extends Activity implements CvCameraViewListener2 {
     private static final String TAG = "OCVSample::Activity";
@@ -23,6 +41,10 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
     private CameraBridgeViewBase mOpenCvCameraView;
     private boolean              mIsJavaCamera = true;
     private MenuItem             mItemSwitchCamera = null;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    private Mat frame;
+    private Mat frameCapturado;
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -54,15 +76,41 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
 
         setContentView(R.layout.tutorial1_surface_view);
 
-        if (mIsJavaCamera)
+        //if (mIsJavaCamera)
             mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_java_surface_view);
-        else
-            mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_native_surface_view);
+        //else
+           // mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial1_activity_native_surface_view);
 
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        Button captura = (Button)findViewById(R.id.captureButton);
+        captura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Tutorial1Activity.this.setContentView(R.layout.captured_layout);
+                //frameCapturado = frame;
+                ImageView iv = (ImageView)findViewById(R.id.originalImage);
+                //if(frameCapturado!=null){
+                  //  Log.e("CAPTURA","frame capturado");
+                //}else{
+                  //  Log.e("CAPTURA","frame no capturado");
+                //}
+                ImageView ev = (ImageView)findViewById(R.id.editedImage);
+                Bitmap img = Bitmap.createBitmap(frame.cols(),
+                        frame.rows(), Bitmap.Config.ARGB_8888);
+                Utils.matToBitmap(frame, img);
+                iv.setImageBitmap(Bitmap.createScaledBitmap(img, img.getWidth() / 2,
+                        img.getHeight() / 2, false));
+                ev.setImageBitmap(Bitmap.createScaledBitmap(img,img.getWidth()/2,
+                        img.getHeight()/2,false));
+
+                //Tutorial1Activity.this.addContentView(iv,null);
+            }
+        });
     }
+
 
     @Override
     public void onPause()
@@ -126,6 +174,10 @@ public class Tutorial1Activity extends Activity implements CvCameraViewListener2
     }
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        //Log.e("CAPTURA","tam inputFrame rgba "+inputFrame.rgba().size());
+        frame = Mat.zeros(inputFrame.rgba().size(), CvType.CV_8UC3);
+        Imgproc.cvtColor(inputFrame.rgba(),frame,Imgproc.COLOR_RGBA2RGB);
+        //frame = inputFrame.gray();
         return inputFrame.rgba();
     }
 }
